1f7a58873eac2c71f7b0686fcffec80f4d4f1f84
Sabir2408 <casper98187@gmail.com>
2023-02-01 07:41:01 +0300

    3

---
+++ main.js
@@ -0,0 +1,41 @@
+//1 задание
+// function getNumber(a, b){
+//     if (a<b){
+//         console.log('меньше');
+//     }else{
+//         console.log('больше');  
+//     }
+// }
+//       getNumber(3, 2)
+//       getNumber(2, 1)
+      
+
+
+//2 задание
+// function twoMass(){
+//     let array1 = prompt ('ввести первое значение')
+//     let array2 = prompt ('ввести второе значение')
+//     if (array1>array2){
+//        alert ('первое значение больше');
+//     }if(array1<array2){
+//         alert('первое значение меньше');
+//     }else{
+//         alert ('значения равны');
+//     }
+// }
+// twoMass()
+
+//3 задание
+// function countChar(){
+//     let a = prompt('введите слово')
+//     alert ('длина слова:' + a.length + 'символов')
+// }
+// countChar()
+ 
+//доп
+function  parametrs(a,b,c) {
+    return (a+b+c);   
+}
+let summa = parametrs(2,3,4);
+console.log(summa);
+
